d767503bb5c609e82ee3ebeba9cfbd5b2b9d1f2a
KasparM8 <kasparmeos8@gmail.com>
2024-01-05 21:26:52 +0200

    add week 3 files

---
+++ index.js
@@ -0,0 +1,33 @@
+const btnSubmit = document.querySelector('button');
+const usernameInputField = document.querySelector('#username');
+const userAgeInputField = document.querySelector('#age');
+const loginMessage = document.querySelector('.login-message')
+
+
+btnSubmit.addEventListener('click', () => {
+    const usernameInput = usernameInputField.value.toLowerCase(); // Harry, haRry
+    const userAgeInput = userAgeInputField.value;
+
+//     if(usernameInput === 'Meos' && userAgeInput === '16') {
+
+//     console.log('welcome');
+//     loginMessage.textContent = 'Welcome, Mr.Meos!'
+//     loginMessage.style.color = 'green'
+
+// } else { 
+
+//     loginMessage.textContent ='You are not Mr.Meos'
+//     loginMessage.style.color = 'red'
+
+
+//         console.log('go away')
+//     }
+      if(usernameInput !== 'Meos' || userAgeInput !== '16' ){
+        loginMessage.textContent ='You are not Mr.Meos'
+     loginMessage.style.color = 'red'
+
+      } else {
+        loginMessage.textContent = 'Welcome, Mr.Meos!'
+        loginMessage.style.color = 'green'
+      }
+});
